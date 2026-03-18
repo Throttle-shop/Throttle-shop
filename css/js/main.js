4e2309260d1f4e4e10ca9c88c02261c22a9bc757
johnsmith5710 <33072899+johnsmith5710@users.noreply.github.com>
2026-03-18 13:16:57 -0400

    Create main.js

---
+++ css/js/main.js
@@ -0,0 +1,6 @@
+document.getElementById('menuToggle').addEventListener('click', () => {
+  document.getElementById('navLinks').classList.toggle('active');
+});
+
+// Simple fake form alert on contact page
+function sendMessage() { alert("Thank you! We'll call you back within 1 business day. 📞 (269) 423-9009"); }
